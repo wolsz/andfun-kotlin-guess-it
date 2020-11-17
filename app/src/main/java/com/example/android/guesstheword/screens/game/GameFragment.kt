4d9953590dc8f7fc1caf99bfbc311c3986e43409
wolsz <wolsz1@gmail.com>
2020-11-17 18:02:34 -0500

    First Commit

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -17,11 +17,13 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
@@ -31,6 +33,7 @@ import com.example.android.guesstheword.databinding.GameFragmentBinding
  */
 class GameFragment : Fragment() {
 
+    private lateinit var viewModel: GameViewModel
     // The current word
     private var word = ""
 
@@ -53,6 +56,8 @@ class GameFragment : Fragment() {
                 false
         )
 
+        Log.i("GameFragment", "Called ViewModelProvider!")
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
         // TODO (04) Create and initialize a GameViewModel, using ViewModelProvider; Add a log
         // statement
 
